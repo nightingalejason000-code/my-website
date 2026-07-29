62799cd1f0eb8826ac5ca71bfe3d5fdb514a72e2
Jason <nightingale000@gmail.com>
2026-07-29 10:21:33 -0400

    removed television frm hobbies lst lne 50

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("hello world");
+let name = "Jason";
+let age = 15;
+let isStudent = true;
+let colors = ["red", "blue"];
+let total = 5 + 3
+let diff = 10 - 4
+let product = 6 * 2
+let multiply = (6 * 99 * 19)
+
+// lne 9
+console.log (multiply)
+// 5 greater than 3 v
+console.log (5 > 3);
+// 5 equal to 5 v
+console.log (5 === 5);
+
+console.log (5 !== 3);
+// lne 3 v
+console.log (age);
+// lne 5
+console.log (colors);
+// lne 4 v
+console.log (isStudent);
+//lne 7 v
+console.log (diff);
+// lne 6
+console.log (total);
+// lne 8
+console.log (product);
