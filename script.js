756b83aa537b86ae664e10f75d0f47f439c3c43a
Jason <nightingale000@gmail.com>
2026-08-03 14:48:55 -0400

    added email lnk and flx-bx to footer

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-console.log("hello world");
-let name = "Jason";
-let age = 15;
-let isStudent = true;
-let colors = ["red", "blue"];
-let total = 5 + 3
-let diff = 10 - 4
-let product = 6 * 2
-let multiply = (6 * 99 * 19)
+// console.log("hello world");
+// let name = "Jason";
+// let isStudent = true;
+// let colors = ["red", "blue"];
+let fav_hobb = ["javascript", "v-games","books"];
+// let total = 5 + 3;
+// let diff = 10 - 4;
+// let product = 6 * 2;
+// let volume = (9 * 5 * 2);
+// let age_1 = (15)
 
-// lne 9
-console.log (multiply)
-// 5 greater than 3 v
-console.log (5 > 3);
-// 5 equal to 5 v
-console.log (5 === 5);
+console.log(fav_hobb)
+// // 5 greater than 3 v
+// console.log (5 > 3);
+// // 5 equal to 5 v
+// console.log (5 === 5);
+// // not equal
+// console.log (5 !== 3);
+// // lne 5
+// console.log (colors);
+// // lne 4 v
+// console.log (isStudent);
+// //lne 7 v
+// console.log (diff);
+// // lne 6 v
+// console.log (total);
+// // lne 10 v
+// console.log (volume)
 
-console.log (5 !== 3);
-// lne 3 v
-console.log (age);
-// lne 5
-console.log (colors);
-// lne 4 v
-console.log (isStudent);
-//lne 7 v
-console.log (diff);
-// lne 6
-console.log (total);
-// lne 8
-console.log (product);
+// function greet (name) {
+//     console.log('Hello', name);
+// }
+// greet ("Jason");
+
+// function double(num) {
+//     return num * 2;
+// }
+
+// let result = double(4);
+// console.log (result);
+
+// let age = 18;
+
+// if (age < 11 ) 
+// { console.log("rslt 1") }
+//     else if (age < 10 ) 
+// { console.log("rslt 2")} 
+//     else 
+// { console.log("rslt 3") } 
+
+// function checkAge(age) 
+// {  if (age >= 18)
+
+// {return "Adult";} 
+//     else 
+// {return "Minor";}
+//   }
+  
+// console.log(checkAge(15));
+// console.log(checkAge(20));
+
